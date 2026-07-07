3955b32eeb60432e6dc6f438f58766ab2eefe744
unknown <iliyaaabdurrazaq@gmail.com>
2026-07-07 12:54:11 +0100

    deleting old files

--- lib/db/dist/schema/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from "./users";
-export * from "./matches";
-export * from "./markets";
-export * from "./positions";
-export * from "./insurance";
-export * from "./proofs";
-export * from "./liquidity";
-export * from "./governance";
-//# sourceMappingURL=index.d.ts.map
